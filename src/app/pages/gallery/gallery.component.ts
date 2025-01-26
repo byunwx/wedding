3c259d1bbd99ae6f039ff92d7857892bdf8f4e5a
Woojoo Byun <wbyun@miso3.com>
2025-01-26 10:26:50 -0500

    add photoes

--- src/app/pages/gallery/gallery.component.ts
+++ src/app/pages/gallery/gallery.component.ts
@@ -8,85 +8,81 @@ import { PictureFrameComponent } from '../../components/picture-frame/picture-fr
   styleUrl: './gallery.component.scss'
 })
 export class GalleryComponent {
-  public pictures: [{
+  public pictures: {
     id: number;
     src: string;
-  }] = [
-        {
+  }[] = [
+    {
       id: 1,
-      src: 'loadingpicture.jpg',
+      src: '001.jpg',
     },
-    // {
-    //   id: 1,
-    //   src: '001.jpg',
-    // },
-    // {
-    //   id: 2,
-    //   src: '002.jpg',
-    // },
-    // {
-    //   id: 3,
-    //   src: '003.jpg',
-    // },
-    // {
-    //   id: 4,
-    //   src: '004.jpg',
-    // },
-    // {
-    //   id: 5,
-    //   src: '005.jpg',
-    // },
-    // {
-    //   id: 6,
-    //   src: '006.jpg',
-    // },
-    // {
-    //   id: 8,
-    //   src: '008.jpg',
-    // },
-    // {
-    //   id: 9,
-    //   src: '009.jpg',
-    // },
-    // {
-    //   id: 10,
-    //   src: '010.jpg',
-    // },
-    // {
-    //   id: 11,
-    //   src: '011.jpg',
-    // },
-    // {
-    //   id: 12,
-    //   src: '012.jpg',
-    // },
-    // {
-    //   id: 13,
-    //   src: '013.jpg',
-    // },
-    // {
-    //   id: 14,
-    //   src: '014.jpg',
-    // },
-    // {
-    //   id: 15,
-    //   src: '015.jpg',
-    // },
-    // {
-    //   id: 16,
-    //   src: '016.jpg',
-    // },
-    // {
-    //   id: 17,
-    //   src: '017.jpg',
-    // },
-    // {
-    //   id: 18,
-    //   src: '018.jpg',
-    // },
-    // {
-    //   id: 19,
-    //   src: '019.jpg',
-    // },
+    {
+      id: 2,
+      src: '002.jpg',
+    },
+    {
+      id: 3,
+      src: '003.jpg',
+    },
+    {
+      id: 4,
+      src: '004.jpg',
+    },
+    {
+      id: 5,
+      src: '005.jpg',
+    },
+    {
+      id: 6,
+      src: '006.jpg',
+    },
+    {
+      id: 8,
+      src: '008.jpg',
+    },
+    {
+      id: 9,
+      src: '009.jpg',
+    },
+    {
+      id: 10,
+      src: '010.jpg',
+    },
+    {
+      id: 11,
+      src: '011.jpg',
+    },
+    {
+      id: 12,
+      src: '012.jpg',
+    },
+    {
+      id: 13,
+      src: '013.jpg',
+    },
+    {
+      id: 14,
+      src: '014.jpg',
+    },
+    {
+      id: 15,
+      src: '015.jpg',
+    },
+    {
+      id: 16,
+      src: '016.jpg',
+    },
+    {
+      id: 17,
+      src: '017.jpg',
+    },
+    {
+      id: 18,
+      src: '018.jpg',
+    },
+    {
+      id: 19,
+      src: '019.jpg',
+    },
   ]
 }
